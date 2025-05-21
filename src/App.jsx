eac9e42c52f2dcdfd108afdca83aabdeb236e96b
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-21 11:39:53 +0200

    [UPD]Add-css-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import SearchBar from "./SearchBar";
+import "./App.css"; // importo css cosi do stile alla pagina per farlo somigliare a netflix
 
-
-function App() {
+function App() {   //creiamo il componente app(react va per componenti)
   return (
-    <div>
-      <h1>BoolFlix</h1>
-      <SearchBar/>
+    <div className="app-container">
+      <header className="app-header">
+        <h1>🎬 BoolFlix</h1>                        {/**titolo pagina  */}
+        <SearchBar />                               {/**questa è la navbar dove mettiamo i film */}
+      </header>
     </div>
   );
 }
 
-export default App;
+export default App;                                 //app vienme esportato e reso disponibile dove serve
+
